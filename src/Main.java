faf53c2f6c4aabea5168c28380a34b3aa0821eb3
saivinay <kanamarlapudisaivinay@gmail.com>
2023-09-01 14:48:12 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] arg)
+    {
+        rectangle Rectangle= new rectangle(10,50);
+
+        System.out.println(Rectangle.calculatearea());
+        System.out.println(Rectangle.calculateperimeter());
+    }
+}
